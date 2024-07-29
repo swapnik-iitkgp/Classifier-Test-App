95cd603b2d630f6993dced6befaf0ab4c0c2580a
Swapnik Varala <swapnikvarala@gmail.com>
2024-07-29 13:43:10 +0530

    done

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import base64
+import json
 import time
 
 # Download necessary NLTK data
@@ -16,46 +17,11 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
+file_path = 'label_info.json'
 
-label_dict = {
-    0: "Irrelevant",
-    1: "Call opening greeting",
-    2: "Call closing greeting",
-    3: "Purpose of Call",
-    4: "Did Rep Ask The Spelling Of Customer Name?",
-    5: "Did Rep Ask The Exact Customer Address?",
-    6: "Was The Policy Disclaimer Readout?",
-    7: "Was The Customer Asked For Secondary & Next Of Kin Contact Numbers?",
-    8: "Was Client Residency Information Taken?",
-    9: "Did Rep ask for Date of Birth?",
-    10: "Was Policy Cover Cost Given?",
-    11: "Policy Benefits & Limits(Exclusion Conditions)",
-    12: "Was Cover Description Given To Customer?",
-    13: "Cancellation of Policy",
-    14: "Call Hold",
-    15: "Did Rep Informed The Customer About Call Transfer?",
-    16: "Did Rep Explain Renewal Notification (Policy Contract)?",
-    17: "Did The Rep Read Out All Accidental Serious Injuries Cover?",
-    18: "Did The Rep Share The Policy Number With The Client?",
-    19: "Provision Of Disclosure Documents (General Advice Only Clients)",
-    20: "Whether Accident & Sickness Policy Cover Disclaimer Readout Under Accident & Sickness Insurance?",
-    21: "Denial of claim-exclusion/condition",
-    22: "Did The Rep Explain The Item Description Needs At The Time Of Claim?",
-    23: "Call Recording Disclaimer",
-    24: "General Advice Warning",
-    25: "Was Policy Cover Confirmation Asked And Confirmed?",
-    26: "Did Rep Say He Will Be Sharing Policy Copy?",
-    27: "Confirmation to start policy",
-    28: "Did The Rep Correctly Get The Travel Date?",
-    29: "Health exclusion",
-    30: "Did Rep Ask About Pre-Existing Condition Of The Client?",
-    31: "Pre-Screening Medical Questionnaire",
-    32: "Estimation of premium disclaimer",
-    33: "Was Travel Policy Cover Disclaimer Readout?",
-    34: "Were Current Bank Account Details Taken?",
-    35: "Duty of disclosure",
-    36: "Consent to proceed"
-}
+# Load the JSON data from the file
+with open(file_path, 'r') as json_file:
+    label_dict = json.load(json_file)
 
 # Custom dataset class
 class CustomDataset(Dataset):
@@ -155,7 +121,16 @@ st.title('Text Classification using BERT')
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
+    encodings = ['utf-8', 'ISO-8859-1', 'latin1']
+    for encoding in encodings:
+        try:
+            df = pd.read_csv(uploaded_file, encoding=encoding)
+            df_dummy = df.copy()
+            break
+        except (UnicodeDecodeError, pd.errors.EmptyDataError) as e:
+            st.write(f"Error loading file with encoding {encoding}: {e}")
+            df = None
+    
     st.write("Uploaded Data")
     st.write(df.head())
 
@@ -175,10 +150,12 @@ if uploaded_file is not None:
                 _, preds = torch.max(outputs.logits, dim=1)
                 predictions.extend(preds.cpu().numpy())
 
-        label_predictions = [label_dict[prediction] for prediction in predictions]
+        label_predictions = [label_dict[str(prediction)] for prediction in predictions]
 
         # Add predictions to the dataframe
         df['predictions'] = label_predictions
+
+        df['sentence'] = df_dummy['sentence']
 
         # Save the results
         results_file = 'results.csv'
